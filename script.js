d78e6690c0436f97a68608f4826d3739112b7f67
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-02-06 20:14:53 -0300

    criando functions

--- script.js
+++ script.js
@@ -1,64 +1,77 @@
 const imagens = document.querySelectorAll('.animais-img img')
 const texto = document.querySelectorAll('.animais-text')
 
-imagens.forEach((imagem, index) => {
-    imagem.addEventListener('click', function () {
-        texto.forEach(item => {
-            item.classList.remove('ativo')
+function trocarTexto() {
+    imagens.forEach((imagem, index) => {
+        imagem.addEventListener('click', function () {
+            texto.forEach(item => {
+                item.classList.remove('ativo')
+            })
+            texto[index].classList.add('ativo')
         })
-        texto[index].classList.add('ativo')
     })
-})
+}
+
+trocarTexto()
 
 
 // -----------------------------------------------------------------------------------------------------
-
 
 const faqAsk = document.querySelectorAll('.faq-content dt')
 const faqAnswer = document.querySelectorAll('.faq-content dd')
 
-faqAsk.forEach(ask => {
-    ask.addEventListener('click', function () {
-        ask.nextElementSibling.classList.toggle('ativo')
+function accordionList() {
+    faqAsk.forEach(ask => {
+        ask.addEventListener('click', function () {
+            ask.nextElementSibling.classList.toggle('ativo')
+        })
     })
-})
+}
+
+accordionList()
 
 
 // -----------------------------------------------------------------------------------------------------
-
 
 const lista = document.querySelectorAll('[href^="#"]')
 
-lista.forEach(item => {
-    item.addEventListener('click', function (event) {
-        event.preventDefault()
-        href = event.currentTarget.getAttribute('href')
-        secao = document.querySelector(href)
-        secao.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+function scrollSuave() {
+    lista.forEach(item => {
+        item.addEventListener('click', function (event) {
+            event.preventDefault()
+            href = event.currentTarget.getAttribute('href')
+            secao = document.querySelector(href)
+            secao.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
         })
     })
-})
+}
+
+scrollSuave()
 
 
 // -----------------------------------------------------------------------------------------------------
-
 
 const section = document.querySelectorAll('.scroll')
 const altura = window.innerHeight * 0.6
 
-window.addEventListener('scroll', function () {
-    section.forEach(item => {
-        const alturaSecao = item.getBoundingClientRect().top
-        if (alturaSecao < altura) {
-            item.classList.add('ativo')
-        }
-        else {
-            item.classList.remove('ativo')
-        }
+function mostrarConteudo() {
+    window.addEventListener('scroll', function () {
+        section.forEach(item => {
+            const alturaSecao = item.getBoundingClientRect().top
+            if (alturaSecao < altura) {
+                item.classList.add('ativo')
+            }
+            else {
+                item.classList.remove('ativo')
+            }
+        })
     })
-})
+}
+
+mostrarConteudo()
 
 
 // -----------------------------------------------------------------------------------------------------
