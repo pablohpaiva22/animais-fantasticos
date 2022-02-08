e54eb3496d3f3ca21c35d5a0bf00ac923f8e96f1
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-02-08 15:43:43 -0300

    arrumando caminhos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-import trocarTexto from "./scripts/trocar-texto.js"
-import accordionList from "./scripts/accordion-list.js"
-import scrollSuave from "./scripts/scroll-suave.js"
-import mostrarConteudo from "./scripts/mostrar-conteudo.js"
-import modal from "./scripts/modal.js"
+import trocarTexto from "./funcoes/trocar-texto.js"
+import accordionList from "./funcoes/accordion-list.js"
+import scrollSuave from "./funcoes/scroll-suave.js"
+import mostrarConteudo from "./funcoes/mostrar-conteudo.js"
+import modal from "./funcoes/modal.js"
 
 trocarTexto()
 accordionList()
