60e1695f10a6485d1b4782bb2d00cf64980cac54
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-03-08 12:34:18 -0300

    alterando nome das variaves

--- js/funcoes/trocar-texto.js
+++ js/funcoes/trocar-texto.js
@@ -1,13 +1,13 @@
 export default function trocarTexto() {
-    const imagens = document.querySelectorAll('.animais-img img')
-    const texto = document.querySelectorAll('.animais-text')
+    const $imagens = document.querySelectorAll('.animais-img img')
+    const $texto = document.querySelectorAll('.animais-text')
 
-    imagens.forEach((imagem, index) => {
+    $imagens.forEach((imagem, index) => {
         imagem.addEventListener('click', function () {
-            texto.forEach(item => {
+            $texto.forEach(item => {
                 item.classList.remove('ativo')
             })
-            texto[index].classList.add('ativo', texto[index].dataset.anime)
+            $texto[index].classList.add('ativo', $texto[index].dataset.anime)
         })
     })
 }
