b6a95980739b365f82472da7b8352d0fa81fcbbc
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-03-08 11:57:18 -0300

    alterando nome das variaveis

--- js/funcoes/drop-down.js
+++ js/funcoes/drop-down.js
@@ -1,23 +1,23 @@
 export default function dropDown() {
-    const sobre = document.querySelector('[data-drop-down="dropDown"]')
-    const card = document.querySelector('.card-dropDown')
+    const $sobre = document.querySelector('[data-drop-down="dropDown"]')
+    const $card = document.querySelector('.card-dropDown')
 
     function showMenu(event) {
         event.preventDefault()
-        card.classList.toggle('dropDown')
+        $card.classList.toggle('dropDown')
         outsideClick(this, function() {
-            card.classList.remove('dropDown')
+            $card.classList.remove('dropDown')
         })
     }
 
     function outsideClick(element, callback) {
-        const html = document.documentElement
-        html.addEventListener('click', outsideClickCallback)
+        const $html = document.documentElement
+        $html.addEventListener('click', outsideClickCallback)
         function outsideClickCallback(event) {
             if(!element.contains(event.target)) 
                 return callback()
         }
     }
 
-    sobre.addEventListener('click', showMenu)
+    $sobre.addEventListener('click', showMenu)
 }
