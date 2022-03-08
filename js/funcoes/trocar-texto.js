92b27e68c309b75c6de8c4afbc7228a43fa94fc5
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-03-08 12:40:14 -0300

    melhorando escrita da funcao

--- js/funcoes/trocar-texto.js
+++ js/funcoes/trocar-texto.js
@@ -1,13 +1,19 @@
 export default function trocarTexto() {
+    
     const $imagens = document.querySelectorAll('.animais-img img')
     const $texto = document.querySelectorAll('.animais-text')
 
     $imagens.forEach((imagem, index) => {
-        imagem.addEventListener('click', function () {
+        
+        function novoTexto() {
             $texto.forEach(item => {
                 item.classList.remove('ativo')
             })
+
             $texto[index].classList.add('ativo', $texto[index].dataset.anime)
-        })
+        }
+
+        imagem.addEventListener('click', novoTexto)
     })
+
 }
