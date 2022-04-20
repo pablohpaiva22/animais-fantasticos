8757b09eb610d538a001d2cfa7c3ac350bdd792a
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-04-20 16:46:29 -0300

    estruturando melhor as pastas

---
+++ src/components/animaisFetch.js
@@ -0,0 +1,19 @@
+export default function animaisFetch() {
+  const container = document.querySelector(".numeros-container");
+
+  async function fetchData(url) {
+    const data = await fetch(url);
+    const dataJSON = await data.json();
+    dataJSON.forEach((animal) => {
+      newElement(animal);
+    });
+  }
+
+  fetchData("../src/data.json");
+
+  function newElement(animal) {
+    const createDiv = document.createElement("div");
+    const div = container.appendChild(createDiv);
+    div.innerHTML = `<div class="numeros-content"><h3>${animal.especie}</h3><span>${animal.quantidade}</span></div>`;
+  }
+}
---
+++ src/script.js
@@ -0,0 +1,29 @@
+import trocarTexto from "./components/trocar-texto.js";
+import AccordionList from "./components/AccordionList.js";
+import ScrollSuave from "./components/ScrollSuave.js";
+import mostrarConteudo from "./components/mostrar-conteudo.js";
+import modal from "./components/modal.js";
+import tooltip from "./components/tooltip.js";
+import dropDown from "./components/drop-down.js";
+import menuMobile from "./components/menu-mobile.js";
+import animaNumeros from "./components/anima-numeros.js";
+import horarioDeAtendimento from "./components/atendimento.js";
+import animaisFetch from "./components/animaisFetch.js";
+import doeBitcoin from "./components/bitcoinFetch.js";
+
+trocarTexto();
+mostrarConteudo();
+modal();
+tooltip();
+dropDown();
+menuMobile();
+animaNumeros();
+horarioDeAtendimento();
+animaisFetch();
+doeBitcoin();
+
+const scrollSuave = new ScrollSuave('[href^="#"]');
+scrollSuave.init();
+
+const accordionList = new AccordionList(".faq-content dt");
+accordionList.init();
