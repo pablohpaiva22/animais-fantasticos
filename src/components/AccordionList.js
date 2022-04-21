7a701338ee39e052dacd0f4129a3a209e27c569a
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-04-20 23:24:00 -0300

    passando eslint

--- src/components/AccordionList.js
+++ src/components/AccordionList.js
@@ -1,21 +1,21 @@
 export default class AccordionList {
-    constructor(list) {
-        this.list = document.querySelectorAll(list)
-    }
+  constructor(list) {
+    this.list = document.querySelectorAll(list);
+  }
 
-    toggleAccordion() {
-        this.nextElementSibling.classList.toggle('ativo')
-    }
+  toggleAccordion() {
+    this.nextElementSibling.classList.toggle("ativo");
+  }
 
-    addEvent() {
-        this.list.forEach(item => {
-            item.addEventListener('click', this.toggleAccordion)
-        })
-    }
+  addEvent() {
+    this.list.forEach((item) => {
+      item.addEventListener("click", this.toggleAccordion);
+    });
+  }
 
-    init() {
-        if(this.list.length) {
-            this.addEvent()
-        }
+  init() {
+    if (this.list.length) {
+      this.addEvent();
     }
+  }
 }
